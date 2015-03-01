a1cf736fdd45ef7c2193f97400ec8dbaaf35743b
kstoriadis <federico.gonzalezc@gmail.com>
2015-03-01 18:36:01 -0300

    Avances
    Rumbo al prototipo para la presentación. Falta landing de calendario,
    pagina de descargas/archivos y armar el footer

--- js/script.js
+++ js/script.js
@@ -1,17 +1,90 @@
+function agrandarCuerpo(alturaContenido) {
+	var altura1 = $("body").height() - $("#header").outerHeight() - $("#footer").outerHeight() - 35;
+
+	//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
+	if (alturaContenido > altura1) {
+		$("#cuerpo").height(alturaContenido);
+	} else {
+		$("#cuerpo").height(altura1);
+	}
+}
+
+function cargarCalendario(cal) {
+	$('.wrapperCalendario').fadeOut('slow', function() {
+
+		$.get(cal, function(msg) {
+			$('.wrapperCalendario').html(msg);			
+			$('.wrapperCalendario').fadeIn('slow');
+			inicializarCalendario();
+		});
+	});
+}
+function inicializarCalendario(){
+	$('.itemNavegacionCalendario').each(function(index) {
+			$(this).click(function() {
+
+				var linkActual = $(this);
+				var linkAnterior = $('.itemNavegacionCalendario.oculto');
+
+				$(this).hide({
+					direction : "right",
+					effect : "drop",
+					duration : 1000,
+					complete : function() {
+						$(linkActual).addClass("oculto");
+					}
+				});
+				$('#tituloCalendario').hide({
+					effect : "fade",
+					duration : 1000,
+					complete : function() {
+						$('#tituloCalendario').html($(linkActual).find(".tituloItemNavegacion").html());
+					}
+				});
+				$('#wrapperIframeCalendario').hide({
+					direction : "down",
+					effect : "drop",
+					duration : 1000,
+					complete : function() {
+						$('#wrapperIframeCalendario').html($(linkActual).find(".oculto").html());
+						$(linkAnterior).removeClass("oculto");
+
+						$(linkAnterior).show({
+							direction : "right",
+							effect : "drop",
+							duration : 1000
+						});
+						$('#tituloCalendario').show({
+							effect : "fade",
+							duration : 1000
+						});
+
+						$('#wrapperIframeCalendario').show({
+							direction : "down",
+							effect : "drop",
+							duration : 1000
+						});
+					}
+				});
+
+			});
+
+		});
+		var alturaIf = $('#wrapperIframeCalendario').height();
+		var anchoIf = $('#wrapperIframeCalendario').width();
+
+		$('#wrapperIframeCalendario').html($('#wrapperIframeCalendario').html().replace('200', anchoIf));
+		$('#wrapperIframeCalendario').html($('#wrapperIframeCalendario').html().replace(/300/g, alturaIf));
+
+		$('.oculto').each(function(index) {
+			$(this).html($(this).html().replace('200', anchoIf).replace(/300/g, alturaIf));
+		});
+}
+
 $(document).ready(function() {
 	var a = window.location.toString();
 	var name = a.indexOf("index");
 	var name2 = a.indexOf("/");
-
-	var altura1 = $("body").height() - $("#header").outerHeight() - $("#footer").outerHeight() - 35;
-
-	var altura = $(".contenidoPrincipal").height() * 1.1 + 150;
-	//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
-	if (altura > altura1) {
-		$("#cuerpo").height(altura);
-	} else {
-		$("#cuerpo").height(altura1);
-	}
 
 	$('.encabezado').click(function() {
 		window.location.href = "index.html";
@@ -26,6 +99,16 @@ $(document).ready(function() {
 			adapatativeHeight : true
 		});
 	}
+	name = a.indexOf("institucional");
+	if (name > 0) {
+		//institucional
+		var altura = $(".contenidoPrincipal").height() * 1.1;
+	} else {
+		//todas menos institucional
+		var altura = $(".contenidoPrincipal").height() + 30;
+	}
+	agrandarCuerpo(altura);
+
 	name = a.indexOf("formacion");
 	if (name > 0) {
 		/*PAGINA DE FORMACION*/
@@ -82,8 +165,12 @@ $(document).ready(function() {
 						$('#wrapperContenidoFormacion').show({
 							direction : "down",
 							effect : "drop",
-							duration : 1000
+							duration : 1000,
+							complete : function() {
+								agrandarCuerpo($("#tituloFormacion").outerHeight() + $("#bajadaFormacion").outerHeight() + $("#hSeparatorFormacion").outerHeight() + $("#wrapperContenidoFormacion").outerHeight() + 70);
+							}
 						});
+
 					}
 				});
 
@@ -95,14 +182,14 @@ $(document).ready(function() {
 		$(".itemNavegacionFormacion[indice*='" + indice + "']").click();
 
 	};
-	name = a.indexOf("calendario");
+	name = a.indexOf("masque");
 	if (name > 0) {
-		/*PAGINA DE CALENDARIO*/
-		$('.itemNavegacionCalendario').each(function(index) {
+		/*PAGINA DE Mas que una escuela*/
+		$('.itemNavegacionFormacion').each(function(index) {
 			$(this).click(function() {
 
+				var linkAnterior = $('.itemNavegacionFormacion.oculto');
 				var linkActual = $(this);
-				var linkAnterior = $('.itemNavegacionCalendario.oculto');
 
 				$(this).hide({
 					direction : "right",
@@ -112,19 +199,26 @@ $(document).ready(function() {
 						$(linkActual).addClass("oculto");
 					}
 				});
-				$('#tituloCalendario').hide({
+				$('#tituloFormacion').hide({
 					effect : "fade",
 					duration : 1000,
 					complete : function() {
-						$('#tituloCalendario').html($(linkActual).find(".tituloItemNavegacion").html());
+						$('#tituloFormacion').html($(linkActual).find(".tituloItemNavegacion").html());
 					}
 				});
-				$('#wrapperIframeCalendario').hide({
+				$('#bajadaFormacion').hide({
+					effect : "fade",
+					duration : 1000,
+					complete : function() {
+						$('#bajadaFormacion').html($(linkActual).find(".bajadaItemNavegacion").html());
+					}
+				});
+				$('#wrapperContenidoFormacion').hide({
 					direction : "down",
 					effect : "drop",
 					duration : 1000,
 					complete : function() {
-						$('#wrapperIframeCalendario').html($(linkActual).find(".oculto").html());
+						$('#wrapperContenidoFormacion').html($(linkActual).find(".oculto").html());
 						$(linkAnterior).removeClass("oculto");
 
 						$(linkAnterior).show({
@@ -132,28 +226,42 @@ $(document).ready(function() {
 							effect : "drop",
 							duration : 1000
 						});
-						$('#tituloCalendario').show({
+						$('#tituloFormacion').show({
+							effect : "fade",
+							duration : 1000
+						});
+						$('#bajadaFormacion').show({
 							effect : "fade",
 							duration : 1000
 						});
 
-						$('#wrapperIframeCalendario').show({
+						$('#wrapperContenidoFormacion').show({
 							direction : "down",
 							effect : "drop",
-							duration : 1000
+							duration : 1000,
+							complete : function() {
+								var altNav = $(".wrapperNavegacionFormacion").outerHeight();
+								var altInfo = $("#tituloFormacion").outerHeight() + $("#bajadaFormacion").outerHeight() + $("#hSeparatorFormacion").outerHeight() + $("#wrapperContenidoFormacion").outerHeight() + 70;
+								agrandarCuerpo(altNav > altInfo ? altNav : altInfo);
+							}
 						});
+
 					}
 				});
 
 			});
 
 		});
-		var alturaIf = $('#wrapperIframeCalendario').height();
-		var anchoIf = $('#wrapperIframeCalendario').width();
-		$('#wrapperIframeCalendario').html($('#wrapperIframeCalendario').html().replace('200', anchoIf).replace('300', alturaIf));
-		$('.oculto').each(function(index) {
-			$(this).html($(this).html().replace('200', anchoIf).replace('300', alturaIf));
-		});
+		var a = window.location.toString();
+		var indice = a.substring(a.indexOf("=") + 1);
+		$(".itemNavegacionFormacion[indice*='" + indice + "']").click();
+
+	};
+
+	name = a.indexOf("calendario");
+	if (name > 0) {
+		/*PAGINA DE CALENDARIO*/
+		inicializarCalendario();
 
 	};
 
